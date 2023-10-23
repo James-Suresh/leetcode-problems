087262002d7cdba8ced8a094daab555c52752ac3
James <dev.jamessuresh@gmail.com>
2023-10-22 22:47:53 -0400

    Time: 1287 ms (7.71%), Space: 34.2 MB (97%) - LeetHub

---
+++ 0883-car-fleet/0883-car-fleet.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        """
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
+
+        """
+        pos_speed = list(zip(position,speed))
+        pos_speed.sort(reverse=True)
+        stack=[]
+        res=0
+
+        print pos_speed
+
+        for i,p in enumerate(pos_speed):
+            t = float(target - p[0])/float(p[1])
+            print t
+            if stack and stack[-1]>=t:
+                continue
+            else:
+                stack.append(t)
+                res+=1
+      
+        return res
+
+        
