366e07b5c3980b7bad93d98de12943cdfb6fd4e7
James <dev.jamessuresh@gmail.com>
2023-11-02 23:58:33 -0400

    Time: 95 ms (5.07%), Space: 13.4 MB (11.08%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        l=0
+        r=len(nums)-1
+        while(l<len(nums)):  
+            print nums
+            print l
+            print r
+            if nums[l]==0:
+                nums.pop(l)
+                nums.insert(0,0)
+           
+            l+=1
+            
+            
+        while r>=0:  
+            print nums
+            print l
+            print r
+            if nums[r]==2:
+                nums.pop(r)
+                nums.append(2)
+           
+            r-=1
+            
+       
+        return nums
+
