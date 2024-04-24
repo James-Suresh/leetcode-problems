2ce6a127dff2af477b01ef4d5c76642740345944
James <dev.jamessuresh@gmail.com>
2024-04-24 14:55:15 -0500

    Time: 162 ms (55.11%), Space: 13.3 MB (43.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -6,10 +6,6 @@ class Solution(object):
         """
         hm = {}
         m=len(nums)/2
-        # if len (nums) % 2 != 0:
-        #     m=(len(nums)/2)+1
-        # else:
-        #     m= len(nums)/2
         for n in nums:
             hm[n] = hm.get(n, 0) + 1
             if hm[n]>m:
